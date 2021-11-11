8fd529d8f5f308509e80a12dc1459ef8e2f8d97b
JoshuaChristopherAkpabio <57246111+JoshuaChristopherAkpabio@users.noreply.github.com>
2021-11-11 10:50:50 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from tkinter import *
+
+
+def miles_km():
+    miles = float(miles_input.get())
+    km = miles * 1.609
+    kilometer_result_label.config(text=f"{km}")
+
+
+window = Tk()
+window.title("Mile to Kilometer Converter")
+window.config(padx=20, pady=20)
+
+miles_input = Entry(width=4)
+miles_input.grid(column=1, row=0)
+
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0, row=1)
+
+kilometer_result_label = Label(text="0")
+kilometer_result_label.grid(column=1, row=1)
+
+kilometer_label = Label(text="Km")
+kilometer_label.grid(column=2, row=0)
+
+calculate_button = Button(text="Calculator", command=miles_km)
+calculate_button.grid(column=1, row=2)
+
+window.mainloop()
